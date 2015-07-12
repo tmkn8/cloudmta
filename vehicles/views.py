142dc8d017f40c1101895bee258f472df2ada1b4
Tomasz Knapik <tmkn0808@gmail.com>
2015-07-12 18:29:51 +0100

    Wypluj 404 gdy gracz nie ma uprawnień do pojazdu

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .models import Vehicle
 
 @login_required
 def vehicles_show(request, pk):
     """Wyświetl stronę o pojeździe"""
     vehicle = get_object_or_404(Vehicle, pk=pk)
-    vehicle.check_permissions(request.user)
+    if not vehicle.check_permissions(request.user):
+        raise Http404()
     return render(request, 'vehicles/show.html', {'vehicle': vehicle})
