af2855d5612f0a7180183e82efee442fe1693455
Tomasz Knapik <tmkn0808@gmail.com>
2015-07-13 21:37:41 +0100

    Podmień 404 z 403 w panelu pojazdów

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
+from django.core.exceptions import PermissionDenied
 from .models import Vehicle
 
 @login_required
@@ -8,5 +8,5 @@ def vehicles_show(request, pk):
     """Wyświetl stronę o pojeździe"""
     vehicle = get_object_or_404(Vehicle, pk=pk)
     if not vehicle.check_permissions(request.user):
-        raise Http404()
+        raise PermissionDenied
     return render(request, 'vehicles/show.html', {'vehicle': vehicle})
